577443daa0bb0242004e174291369789496eacd0
unknownMusician <unknown.musician.t@gmail.com>
2025-07-17 20:59:23 +0300

    cleanup publishing

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+
+pub mod prelude {
+    pub use fruits_prelude::*;
+}
+
+pub mod modules {
+    pub use fruits_modules::*;
+}
+
+pub mod utils {
+    pub use fruits_utils::*;
+}
+
+pub mod math {
+    pub use fruits_math::*;
+}
